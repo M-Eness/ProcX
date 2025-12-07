cd402643bdb798733d6e3889a0093544bd67b993
Enes Erden <mehmeteneserden@gmail.com>
2025-12-07 14:07:31 +0300

    Added init_shared_memory funciton

--- procx.c
+++ procx.c
@@ -33,11 +33,13 @@ typedef struct {
     time_t start_time; // Başlangıç zamanı
     int is_active; // Aktif mi? (1: Evet, 0: Hayır)
 } ProcessInfo;
+
 // Paylaşılan bellek yapısı
 typedef struct {
     ProcessInfo processes[50]; // Maksimum 50 process
     int process_count; // Aktif process sayısı
 } SharedData;
+
 // Mesaj yapısı
 typedef struct {
     long msg_type; // Mesaj tipi
@@ -66,7 +68,53 @@ int parse_command(char *line, char **argv, int max_args, int *detached) {
     return argc;
 }
 
+SharedData* init_shared_memory() {
+    // Shared Memory
+    int shm_fd = shm_open("/procx_shm", O_RDWR, 0666);
+    int is_first = 0;
+
+    if (shm_fd == -1) {
+        shm_fd = shm_open("/procx_shm",O_CREAT | O_RDWR, 0666);
+        if(shm_fd == -1) {
+            perror("shm_open hatası");
+            exit(1);
+        }
+        is_first = 1;
+    }
+
+    // Boyutu düzenle
+    if(is_first) {
+        if(ftruncate(shm_fd, sizeof(SharedData)) == -1) {
+            perror("ftruncate hatası");
+            close(shm_fd);
+            exit(1);
+        }
+    }
+
+    // Mapping işlemi
+    SharedData *shared_memory = mmap(NULL, sizeof(SharedData), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    if(shared_memory == MAP_FAILED) {
+        perror("mmap hatası");
+        close(shm_fd);
+        exit(1);
+    }
+
+    if(is_first) {
+        shared_memory->process_count = 0;
+        printf("İlk process oluşturuldu\n");
+    }else {
+        printf("Shared Memorye dahil olundu\n");
+    }
+
+    printf("Mevcut process sayısı: %d\n", shared_memory->process_count);
+    close(shm_fd);
+    return shared_memory;
+}
+
 int main(int argc, char *argv[], char **envp) {
+
+    SharedData* shared_memory = init_shared_memory();
+    shm_unlink("/procx_shm"); // Geçici (her başlangıçta baştan oluşturmak için)
 
 
     return 0;
